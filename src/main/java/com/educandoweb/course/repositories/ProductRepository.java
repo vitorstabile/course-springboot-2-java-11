1fe5a41767de14ddbc500d48a213774c03b36c8b
Vitor Garcia <vitor.stabile@hotmail.com>
2020-07-25 18:15:16 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/repositories/ProductRepository.java
+++ src/main/java/com/educandoweb/course/repositories/ProductRepository.java
@@ -2,7 +2,6 @@ package com.educandoweb.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
